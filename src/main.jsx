9e94483b0526caebc26ae38fd31f2d69d05f567b
marixane <prof.marwane@gmail.com>
2026-07-04 15:56:26 +0100

    Import cahier de texte styles

--- src/main.jsx
+++ src/main.jsx
@@ -85,5 +85,6 @@ import './mobile-phone-force.js';
 import './mobile-touch-drag.js';
 import './mobile-exercice-touch-zone.css';
 import './line-grid-mode.js';
+import './cahier-texte.css';
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
